4b4e4e0c495e6290ede940239cb294f789b72cf3
AugusMeow <bramlye@outlook.com>
2024-09-08 20:49:05 +0800

    feat: python

--- get-by-link.sh
+++ get-by-link.sh
@@ -16,12 +16,22 @@ echo "get $asset_name"
 case $asset_name in
 "ffmpeg")
     asset_url="https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z"
-    extension=$(echo "$asset_url" | awk -F'.' '{print $NF}')
-    asset_name=$asset_name.$extension
-    echo ">>> get $asset_name from $asset_url"
-    wget -q $asset_url -O $asset_name
+    ;;
+"python")
+    asset_url="https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe"
     ;;
 *)
     echo "Sorry, your asset $asset_name is not listed."
     ;;
 esac
+
+# 检查空字符串
+if [[ -z $asset_url ]]; then
+    echo "ASSET URL IS NOT EXIST!!!"
+    exit 1
+fi
+
+extension=$(echo "$asset_url" | awk -F'.' '{print $NF}')
+asset_name=$asset_name.$extension
+echo ">>> get $asset_name from $asset_url"
+wget -q $asset_url -O $asset_name
